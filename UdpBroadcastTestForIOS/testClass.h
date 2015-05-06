07920791d732ccc84bc522727934bfd6faf43034
FergusChen <ferguschen@FergusChendeMac.local>
2015-05-06 14:38:53 +0800

    Version v1

---
+++ UdpBroadcastTestForIOS/testClass.h
@@ -0,0 +1,13 @@
+//
+//  testClass.h
+//  UdpBroadcastTestForIOS
+//
+//  Created by FergusChen on 15/5/6.
+//  Copyright (c) 2015年 sseOfUSTC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testClass : NSObject
+
+@end
---
+++ UdpBroadcastTestForIOS/testClass.m
@@ -0,0 +1,13 @@
+//
+//  testClass.m
+//  UdpBroadcastTestForIOS
+//
+//  Created by FergusChen on 15/5/6.
+//  Copyright (c) 2015年 sseOfUSTC. All rights reserved.
+//
+
+#import "testClass.h"
+
+@implementation testClass
+
+@end
